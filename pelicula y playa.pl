a703897c8bae6b2110d4fd4d689dbd9de78dd13d
KillerCode_236 <45013989+Rubencrash236@users.noreply.github.com>
2021-07-26 09:40:16 -0400

    peliculas y playas

---
+++ pelicula y playa.pl
@@ -0,0 +1,19 @@
+playa('playa rincon', 'las galeras').
+playa('cayo levantado','samana').
+playa('isla saona', 'isla saona').
+playa('bavaro beach', 'bavaro').
+playa('cayo arena', 'punta rucia').
+playa('playa fronton', 'las galeras').
+playa('playa coson', 'las terrenas').
+playa('playa grande', 'rio san juan').
+playa('lanza del norte','las galeras').
+
+pelicula(luca,comedia,1.36).
+pelicula('black widow', accion,2.14).
+pelicula(cruela, comedia, 2.14).
+pelicula('raya and the last dragon', aventura, 1.57).
+pelicula(after, romance,1.45).
+pelicula(emma, melodrama, 2.12).
+pelicula('the seventh day', horror,1.27).
+pelicula('the conjuring: the devil made me do it',horror, 1.52).
+pelicula(oxygen,'sci-fi',1.41).
