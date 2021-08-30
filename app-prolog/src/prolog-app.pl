974380c2a401ecfd0dc686887f5bdbbab692e459
Felix Guzman <49173087+Po1arM@users.noreply.github.com>
2021-08-30 10:13:40 -0400

    Update prolog-app.pl

--- app-prolog/src/prolog-app.pl
+++ app-prolog/src/prolog-app.pl
@@ -1,13 +1,11 @@
 :- dynamic precio/2.
 :- dynamic total/1.
-:- dynamic capital/1.
 
 precio('elevado',2000).
 precio('medio',1500).
 precio('economico',500).
 
 total(0).
-capital(0).
 
 local(sbg,restaurante,'santo domingo',excelente).
 local(shushi_bali,restaurante,duarte,alta).
@@ -18,8 +16,6 @@ tipoComida(el_cayo,pescados_y_mariscos,1500).
 tipoComida(sbg,gourmet,2500).
 tipoComida(shushi_bali,japonesa,1000).
 tipoComida(el_fogon,criolla,100).
-
-
 
 local(drink_king,bar,espalliat,media).
 local(la_esquina_de_chalo,bar,'puerto plata',alta).
@@ -36,13 +32,10 @@ local(star_bucks,cafe,'santo domingo',alta).
 local(el_polo,cafe,duarte,media).
 local(segafredo_zanetti,cafe,'la altagracia',baja).
 
-
-%hotel(nombre, [servicios], ubicacion, valoracion, precio)
-
-hotel('hilton',[wifi,piscina,estacionamiento,spa,restaurante,bar,gym],'bayahibe',8,3,4284).
-hotel('casa de campo',[wifi,alberca,spa,restaurante,bar,gym],'la romana',9,5,206).
-hotel('hyatt',[piscina,spa,restaurante,bar,gym],'bavaro',9,5,511).
-hotel('gran jimenoa',[piscina,spa,estacionamiento,mascotas,restaurante,bar],'jarabacoa','8',2,52).
+hotel('hilton',[wifi,piscina,estacionamiento,spa,restaurante,bar,gym],'la altagracia',alta,3,4284).
+hotel('casa de campo',[wifi,alberca,spa,restaurante,bar,gym],'la romana',excelente,5,206).
+hotel('hyatt',[piscina,spa,restaurante,bar,gym],'la altagracia',excelente,5,511).
+hotel('gran jimenoa',[piscina,spa,estacionamiento,mascotas,restaurante,bar],'la vega',alta,2,52).
 
 actCultural(yago_yo_no_soy_el_que_soy,teatro,'santo domingo',500).
 actCultural(terapia,teatro,santiago,1000).
@@ -65,16 +58,6 @@ playa('playa fronton', 'las galeras').
 playa('playa coson', 'las terrenas').
 playa('playa grande', 'rio san juan').
 playa('lanza del norte','las galeras').
-/*
-pelicula(luca,comedia,1.36).
-pelicula('black widow', accion,2.14).
-pelicula(cruela, comedia, 2.14).
-pelicula('raya and the last dragon', aventura, 1.57).
-pelicula(after, romance,1.45).
-pelicula(emma, melodrama, 2.12).
-pelicula('the seventh day', horror,1.27).
-pelicula('the conjuring: the devil made me do it',horror, 1.52).
-pelicula(oxygen,'sci-fi',1.41).*/
 
 pelicula(luca,comedia,1.36).
 pelicula('black widow', accion,2.14).
@@ -148,17 +131,40 @@ monumento('fortaleza san luis','santiago').
 monumento('fortaleza de san felipe','puerto plata').
 
 cerca(Location,Location).
-cerca('punta cana',higuey).
-cerca('punta cana','san rafael').
-cerca('punta cana','bavaro').
-cerca('bavaro',higuey).
-cerca('bavaro','san rafael').
-cerca('san francisco',tenares).
-cerca(santiago,moca).
-cerca(moca,tenares).
-cerca(moca,'san francisco').
-cerca(samana,'las terrenas').
-cerca(samana,'las galeras').
+cerca('la altagracia','la romana').
+cerca('la altagracia','el seibo').
+cerca('la romana','san pedro de macoris').
+cerca('la romana','el seibo').
+cerca('san pedro de macoris','hato mayor').
+cerca('san pedro de macoris','santo domingo').
+cerca('santo domingo','monte plata').
+cerca('monte plata','samana').
+cerca('samana','duarte').
+cerca('duarte','maria trinidad sanchez').
+cerca('hermanas mirabal', 'espalliat').
+cerca('espalliat','santiago').
+cerca('santiago','puerto plata').
+cerca('santiago','valverde').
+cerca('valverde','montecristi').
+cerca('montecristi','dajabon').
+cerca('montecristi','santiago rodriguez').
+cerca('dajabon','elias piña').
+cerca('elias piña','san juan').
+cerca('elias piña','san juan').
+cerca('elias piña','independencia').
+cerca('independencia','bahoruco').
+cerca('independencia','pedernales').
+cerca('pedernales','barahona').
+cerca('barahona','azua').
+cerca('san jose de ocoa','azua').
+cerca('san jose de ocoa','peravia').
+cerca('san jose de ocoa','monseñor nouel').
+cerca('sanchez ramirez','monseñor nouel').
+cerca('san cristobal','monte plata').
+cerca('san cristobal','peravia').
+cerca('santo domingo','monte plata').
+cerca('distrito nacional', 'santo domingo').
+
 
 % itera sobre las pelis de un cinema y devuelve el cine si este contiene el tipo de pelicula que se quiere
 movieType([]).
@@ -183,7 +189,7 @@ lugaresCercanos(Location,Cercanos):- bagof(Location1, cerca(Location,Location1),
 
 % Busca un restaurante dado sus atributos
 searchRestaurante(Name,Location,FoodType,BudgetType,Stars):-
-    local(Name,restaurante,Location,Stars),tipoComida(Name,FoodType,Price),clasificacionPrecio(Price,Type), Type = BudgetType,total(X),capital(Y),Y =< x + Price.
+    local(Name,restaurante,Location,Stars),tipoComida(Name,FoodType,Price),clasificacionPrecio(Price,Type), Type = BudgetType,total(X).
 
 % Busca todos los restaurantes dadas sus caracteristicas
 
@@ -210,12 +216,12 @@ getHoteles(Ubicacion,Puntuacion,Estrellas,TipoPrecio,Servicios,Result):- findall
 checkHoteles(Nombre, Servicios, Ubicacion, Valoracion,Estrellas,TipoPrecio):-
                                      hotel(Nombre, ServiciosHotel, Ubicacion, Valoracion,Estrellas,PrecioHotel),
                                      clasificacionPrecio(PrecioHotel,TipoPrecio),
-                                     subset(Servicios,ServiciosHotel),total(X),capital(Y),Y =< X+PrecioHotel.
+                                     subset(Servicios,ServiciosHotel),total(X).
 
 getActividades(Tipo,Ubicacion,TPrecio,Result):-findall([Nombre,Tipo,Ubicacion,Precio],
                                                auxActividades(Nombre,Tipo,Ubicacion,TPrecio,Precio),
                                                Result).
-auxActividades(Nombre,Tipo,Ubicacion,TPrecio,Precio):- actCultural(Nombre,Tipo,Ubicacion,Precio),clasificacionPrecio(Precio,TPrecio),total(X),capital(Y),Y=< X + Precio.
+auxActividades(Nombre,Tipo,Ubicacion,TPrecio,Precio):- actCultural(Nombre,Tipo,Ubicacion,Precio),clasificacionPrecio(Precio,TPrecio),total(X).
 
 modificarPrecio(Precio,'economico'):- retract(precio('economico',_)),asserta(precio('economico',Precio)).
 modificarPrecio(Precio,'medio'):- retract(precio('medio',_)),asserta(precio('medio',Precio)).
